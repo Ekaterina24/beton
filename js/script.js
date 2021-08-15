61a58110ee8a69b10484e609d8d4d622fb6eda10
Ekaterina Rykova <katrykova2002@yandex.ru>
2021-08-15 23:41:19 +0300

    reviews + contacts + partners

--- js/script.js
+++ js/script.js
@@ -16,4 +16,36 @@ document.querySelectorAll('#menu > *').forEach((item) => {
         collapsible: true,
         active: false
     });
+
+    $('.slides').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+
 });
